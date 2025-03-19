f0ef0d2cfed92fcf439ef41f6a6af03bdcac68d4
Lukas Keller <luk.keller94@gmail.com>
2025-03-19 14:30:29 +0100

    added advanced test

--- test/test_example.py
+++ test/test_example.py
@@ -9,3 +9,16 @@ def test_sudoku_solver(beginner_puzzle):
     # simply call the function in local() mode to bypass modal
     solution = solve.local(p_1.sudoku)
     assert solution.solution == p_1.solution
+
+
+def test_sudoku_solver_advanced(advanced_puzzle):
+
+    for i, p in enumerate(advanced_puzzle):
+        if i>3:
+            break
+        p = advanced_puzzle[0]
+
+        # simply call the function in local() mode to bypass modal
+        solution = solve.local(p.sudoku)
+        assert solution.solution == p.solution
+
